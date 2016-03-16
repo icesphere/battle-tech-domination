45ed63c02b78b9ccedcd0404870b8729d0387bad
icesphere <icesphere12@gmail.com>
2016-03-16 07:01:49 -0600

    updates to game page

--- src/main/java/org/smartreaction/battletechdomination/model/Game.java
+++ src/main/java/org/smartreaction/battletechdomination/model/Game.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class Game {
-    public static final boolean SHOW_GAME_LOG = false;
+    public static final boolean SHOW_GAME_LOG = true;
 
     private String gameId;
 
